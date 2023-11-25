50a93c50ec6681cdb10dcfb8de30abe4d718eb77
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-11-25 14:27:10 +1100

    Update setup

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 setup(
     name="pytest-subinterpreter",
     packages=["pytest_subinterpreter"],
+    python_requires=">=3.13",
+    install_requires=["pytest>=7.0.0"],
     # the following makes a plugin available to pytest
     entry_points={"pytest11": ["subinterpreter = pytest_subinterpreter.plugin"]},
     # custom PyPI classifier for pytest plugins
